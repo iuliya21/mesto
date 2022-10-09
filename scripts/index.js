2e0f73fa79c6a89a0021368fbe1a0079158ea548
iuliya21 <iuliya21@mail.ru>
2022-10-10 00:56:29 +0300

    fix: Исправить ошибки после первой итерации

---
+++ scripts/index.js
@@ -0,0 +1,42 @@
+const ModalActiveClass = "popup_opened";
+
+const openModalBtn = document.querySelector(".profile__button-pencil");
+const modal = document.querySelector(".popup");
+const formElement = document.querySelector(".popup__form");
+const closeModalBtn = modal.querySelector(".popup__button-close");
+const nameText = document.querySelector(".profile__title");
+const jobText = document.querySelector(".profile__paragraph");
+const nameInput = formElement.querySelector(".popup__form-text_input_name");
+const jobInput = formElement.querySelector(".popup__form-text_input_job");
+
+//Функция установки в текстовые поля формы имени и рода деятельности первого исследователя
+function setInput() { 
+  nameInput.value = nameText.textContent;
+  jobInput.value = jobText.textContent;
+}
+
+//Функция закрытия окна, при нажатии на крестик
+function closeModal() {
+  modal.classList.remove(ModalActiveClass);
+}
+
+//Функция, которая сохраняет введенные значения в форму и закрывает её
+function submitHandlerForm (evt) {
+  evt.preventDefault();
+  nameText.textContent = nameInput.value;
+  jobText.textContent = jobInput.value;
+  closeModal();
+}
+//Слушатель на кнопку карандаша, который при клике на карандаш, открывает окно формы
+openModalBtn.addEventListener("click", () => {
+  modal.classList.add(ModalActiveClass);
+  setInput();
+});
+
+//Слушатель на кнопку-крестик, который запускает функцию закрытия формы
+closeModalBtn.addEventListener("click", () => {
+  closeModal();
+});
+
+//Отправка формы при событии sumbit
+formElement.addEventListener("submit", submitHandlerForm);
